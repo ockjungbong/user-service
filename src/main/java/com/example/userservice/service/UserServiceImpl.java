a15a5723a935c02c5970a693bc54eef9e14b4378
devock <ockjungbong@gmail.com>
2022-04-09 21:28:45 +0900

    스프링 시큐리티 최초 적용 - BCryptPasswordEncoder

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.userservice.dto.UserDto;
@@ -13,10 +14,11 @@ import com.example.userservice.jpa.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor	// final이나 @Notnull 어노테이션 붙은 필드의 생성자를 자동 생성해 주는 어노테이션.
 public class UserServiceImpl implements UserService{
 	
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -26,7 +28,7 @@ public class UserServiceImpl implements UserService{
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity = mapper.map(userDto, UserEntity.class);
-		userEntity.setEncryptedPwd("encrypted_password");
+		userEntity.setEncryptedPwd(passwordEncoder.encode(userDto.getPwd()));
 		
 		userRepository.save(userEntity);
 		
